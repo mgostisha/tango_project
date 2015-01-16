76be3ff9fe118b80998eff8cf80a89cf547d9844
Martin Gostisha <martingostisha@gmail.com>
2015-01-15 23:13:29 -0600

    Messing with pillow stuff

--- rango/models.py
+++ rango/models.py
@@ -36,7 +36,7 @@ class UserProfile(models.Model):
 
 	# Additional attributes
 	website = models.URLField(blank=True)
-	# picture = models.ImageField(upload_to="profile_images", blank=True)
+	picture = models.ImageField(upload_to="profile_images", blank=True)
 
 	def __unicode__(self):
 		return self.user.username
